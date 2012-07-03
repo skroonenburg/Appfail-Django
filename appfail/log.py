8c9095d36234e28950e49779571ffa0d851ebc8c
Brenton Cleeland <b.cleeland@gmail.com>
2012-07-03 17:39:23 +1000

    Print JSON dump

--- appfail/log.py
+++ appfail/log.py
@@ -1,21 +1,13 @@
 import logging
 import traceback
+
 import json
-from datetime import datetime
 import uuid
+from datetime import datetime
 from socket import gethostname
 
 from django.conf import settings
 from django.views.debug import ExceptionReporter, get_exception_reporter_filter
-
-# Make sure a NullHandler is available
-# This was added in Python 2.7/3.2
-try:
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
 
 # Make sure that dictConfig is available
 # This was added in Python 2.7/3.2
@@ -25,14 +17,6 @@ except ImportError:
     from django.utils.dictconfig import dictConfig
 
 getLogger = logging.getLogger
-
-# Ensure the creation of the Django logger
-# with a null handler. This ensures we don't get any
-# 'No handlers could be found for logger "django"' messages
-logger = getLogger('django')
-if not logger.handlers:
-    logger.addHandler(NullHandler())
-
 # /end taken from django codebase
 
 
@@ -44,17 +28,18 @@ class AppFailHandler(logging.Handler):
     http://support.appfail.net/kb/rest-api-for-reporting-failures/documentation-of-submission-format-version-1
     """
     
-    def __init__(self):
+    def __init__(self, api_key="dYCk5eQl6MK7DlA7c2cLVQ"):
         logging.Handler.__init__(self)
+        self.api_key = api_key
     
     def emit(self, record):
         send = {}
-        
+    
         if record.exc_info:
-            stack_trace = '\n'.join(traceback.format_exception(*record.exc_info))
+            stack_trace = traceback.format_exc(record.exc_info)
         else:
             stack_trace = "No stack trace available"
-            
+        
         send['ExceptionType'] = record.getMessage()
         send['StackTrace'] = stack_trace
         send['HttpVerb'] = record.request.method
@@ -66,8 +51,10 @@ class AppFailHandler(logging.Handler):
         send['User'] = record.request.user.username
         send['PostValuePairs'] = record.request.POST
         send['QueryValuePairs'] = record.request.GET
-        send['ServerVariable'] = record.request.session,
+        send['ServerVariable'] = record.request.session.items(),
         send['Cookies'] = record.request.COOKIES
-        send['UniqueId'] = uuid.uuid1()         # check if this is OK
+        send['UniqueId'] = str(uuid.uuid1())         # check if this is OK
         send['UserAgent'] = record.request.META.get("HTTP_USER_AGENT")
         send['MachineName'] = gethostname()
+    
+        print json.dumps(send)        
